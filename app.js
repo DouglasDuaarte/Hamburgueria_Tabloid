6d15cc9650d1f6b671121e156eb53454cac3730f
Gabriel Araujo <gabrielaraujo.pessoal@outlook.com.br>
2025-06-10 23:49:58 -0300

    fix node

--- app.js
+++ app.js
@@ -1,19 +1,18 @@
 const express = require('express');
 const path = require('path');
-const bodyParser = require('body-parser');
 
 const app = express();
 
 
 // Configuração do EJS
-app.set('view engine', 'ejs');
+
 app.set('views', path.join(__dirname, 'views'));
+
+app.set('view engine', 'ejs');
 
 // Para servir arquivos estáticos (CSS, JS, imagens)
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Para ler dados do formulário (POST)
-app.use(bodyParser.urlencoded({ extended: true }));
 
 // Rota para a página inicial
 app.get('/', (req, res) => {
@@ -22,16 +21,15 @@ app.get('/', (req, res) => {
 
 // Rota para a página de login
 app.get('/login', (req, res) => {
-  res.render('/views/pages/login');
+  res.render('pages/login');
 });
 
 // Rota para a página de registro
-app.get('/login', (req, res) => {
-  res.render('/views/pages/register');
+app.get('/register', (req, res) => {
+  res.render('pages/register');
 });
 
 // Inicia o servidor
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Servidor rodando em http://localhost:${PORT}`);
+app.listen(3000, () => {
+  console.log('Servidor rodando na porta 3000');
 });
